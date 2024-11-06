12b10a71bd8f2a76197f68055cf456077317d200
Martin Smith <mjsmith673@gmail.com>
2024-11-06 11:49:13 -0500

    Add: Page for displaying books on loan to users and admin controls

--- catalog/models.py
+++ catalog/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.urls import reverse # Used in get_absolute_url() to get URL for specified ID
 from django.db.models import UniqueConstraint # Constrains fields to unique values
 from django.db.models.functions import Lower # Returns lower cased value of field
+from django.conf import settings
+from datetime import date
 import uuid # Required for unique book instances
 
 
@@ -104,6 +106,8 @@ class BookInstance(models.Model):
     imprint = models.CharField(max_length=200)
     due_back = models.DateField(null=True, blank=True)
 
+    borrower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
+
     LOAN_STATUS = (
         ('m', 'Maintenance'),
         ('o', 'On loan'),
@@ -125,6 +129,11 @@ class BookInstance(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
 
 
 class Author(models.Model):
